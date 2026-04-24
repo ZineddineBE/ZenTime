cbca9a35938ed004f79a08bf9d6f550bc0b0faf7
ZineddineBE <beouche.zineddine@gmail.com>
2026-04-24 11:49:21 +0200

    fix: Mise à jour de db.ts

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,16 +1,7 @@
-import { PrismaClient } from "./client/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const adapter = new PrismaMariaDb({
-	host: process.env.DB_HOST,
-	port: Number(process.env.DB_PORT),
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-});
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
